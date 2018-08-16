10a225c373927dc241fd09a4125cd68e58f0a2f4
srai <rai12345suman@gmail.com>
2018-08-16 13:02:36 +0800

    display concordance in UI

--- concordance/static/music/Scripts/Utils.js
+++ concordance/static/music/Scripts/Utils.js
@@ -5,8 +5,8 @@ var corpus = {
         $.ajax({
             method  : "GET",
             url     :"http://localhost:8000/concordance/",
-            done    : function(data) {
-                        console.log(data)
+            success    : function(data) {
+                        $("#concordance_display").text(data);
                     }
         });
     }
