e8e855cd9419049479ddd86f3cd264f64c1f597d
srai <rai12345suman@gmail.com>
2018-08-21 11:22:35 +0800

    new base path edited

--- LearnerCorpus/settings.py
+++ LearnerCorpus/settings.py
@@ -123,11 +123,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'concordance/static/'
 
 SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
 STATICFILES_DIRS = (
-    os.path.join(SITE_ROOT, 'static/'),
+    os.path.join(SITE_ROOT, 'concordance/static/'),
 )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'corpus_files')
