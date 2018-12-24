4595efd4f14f940b085ed7c3728137342bb00726
Suman Rai <rai12345suman@gmail.com>
2018-12-24 15:28:44 +0800

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -5,8 +5,6 @@
  apt-get -y upgrade
  apt-get install -y python3.7
  apt install -y python3-pip
- apt-get install -y git
- apt-get install -y curl  
  pip3 install -r requirements.txt --user 
  
  echo "Start Server"
