114b97c1b76d54058b227ce5076c0e10910d52b8
Suman Rai <rai12345suman@gmail.com>
2018-12-24 15:05:43 +0800

    Create runme.sh

---
+++ runme.sh
@@ -0,0 +1,10 @@
+  #!/bin/bash
+ 
+ echo "Install required Tools"
+ apt-get update
+ apt-get -y upgrade
+ apt-get install -y python3.7
+ apt install -y python3-pip
+ apt-get install -y git
+ apt-get install -y curl
+ 
