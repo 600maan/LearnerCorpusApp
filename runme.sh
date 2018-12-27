1668bc00c0778604ce74bd564e017f4daa408258
Suman Rai <rai12345suman@gmail.com>
2018-12-27 10:19:16 +0800

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -1,12 +1,16 @@
   #!/bin/bash
  
- echo "Install required Tools"
+ echo "Install Python 3.7 and required dependencies"
  apt-get update
  apt-get -y upgrade
  apt-get install -y python3.7
  apt install -y python3-pip
  pip3 install -r requirements.txt --user 
  
- echo "Start Server"
+ echo "Install MongoDB"
+ apt-get install -y mongodb
+ 
+ 
+ echo "Start Python Application"
  python3 manage.py runserver localhost:80
  
